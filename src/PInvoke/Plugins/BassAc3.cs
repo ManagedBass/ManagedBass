d75af79f46e29834d5913404ad02dd3c903f84ca
Mathew Sachin <Mathew.Sachin.Git@Outlook.com>
2016-05-15 14:20:25 +0530

    Plugins using T4 Templates
    * Added Plugin classes and a Plugins Dummy project for hosting T4 Templates.
    * All assemblies are now named as ManagedBass.
      This would enable the usage of Bait and Switch PCL trick with NuGet.

---
+++ src/PInvoke/Plugins/BassAc3.cs
@@ -0,0 +1,103 @@
+﻿
+
+
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace ManagedBass
+{
+    /// <summary>
+    /// Wraps BassAc3
+    /// </summary> 
+    /// <remarks>
+    /// Supports .ac3
+    /// </remarks>
+    public static  class BassAc3
+    {
+#if __IOS__
+        const string DllName = "__internal";
+#else
+        const string DllName = "bass_ac3";
+#endif
+
+#if __ANDROID__ || WINDOWS || LINUX || __MAC__
+        static IntPtr hLib;
+        
+        /// <summary>
+        /// Load from a folder other than the Current Directory.
+        /// <param name="Folder">If null (default), Load from Current Directory</param>
+        /// </summary>
+        public static void Load(string Folder = null) => hLib = DynamicLibrary.Load(DllName, Folder);
+
+        public static void Unload() => DynamicLibrary.Unload(hLib);
+#endif
+
+        public static readonly Plugin Plugin = new Plugin(DllName);
+		
+        
+        /// <summary>
+        /// Enable Dynamic Range Compression (default is false).
+        /// </summary>
+        public static bool DRC
+        {
+            get { return Bass.GetConfigBool(Configuration.AC3DynamicRangeCompression); }
+            set { Bass.Configure(Configuration.AC3DynamicRangeCompression, value); }
+        }
+
+
+        [DllImport(DllName, CharSet = CharSet.Unicode)]
+        static extern int BASS_AC3_StreamCreateFile(bool mem, string file, long offset, long length, BassFlags flags);
+
+        [DllImport(DllName)]
+        static extern int BASS_AC3_StreamCreateFile(bool mem, IntPtr file, long offset, long length, BassFlags flags);
+
+        public static int CreateStream(string File, long Offset = 0, long Length = 0, BassFlags Flags = BassFlags.Default)
+        {
+            return BASS_AC3_StreamCreateFile(false, File, Offset, Length, Flags | BassFlags.Unicode);
+        }
+
+        public static int CreateStream(IntPtr Memory, long Offset, long Length, BassFlags Flags = BassFlags.Default)
+        {
+            return BASS_AC3_StreamCreateFile(true, new IntPtr(Memory.ToInt64() + Offset), 0, Length, Flags);
+        }
+
+        public static int CreateStream(byte[] Memory, long Offset, long Length, BassFlags Flags)
+        {
+            var GCPin = GCHandle.Alloc(Memory, GCHandleType.Pinned);
+
+            var Handle = CreateStream(GCPin.AddrOfPinnedObject(), Offset, Length, Flags);
+
+            if (Handle == 0) GCPin.Free();
+            else Bass.ChannelSetSync(Handle, SyncFlags.Free, 0, (a, b, c, d) => GCPin.Free());
+
+            return Handle;
+        }
+        
+        [DllImport(DllName)]
+        static extern int BASS_AC3_StreamCreateFileUser(StreamSystem system, BassFlags flags, [In, Out] FileProcedures procs, IntPtr user);
+
+        public static int CreateStream(StreamSystem system, BassFlags flags, FileProcedures procs, IntPtr user = default(IntPtr))
+        {
+            var h = BASS_AC3_StreamCreateFileUser(system, flags, procs, user);
+
+            if (h != 0)
+                Extensions.ChannelReferences.Add(h, 0, procs);
+
+            return h;
+        }
+
+        [DllImport(DllName, CharSet = CharSet.Unicode)]
+        static extern int BASS_AC3_StreamCreateURL(string Url, int Offset, BassFlags Flags, DownloadProcedure Procedure, IntPtr User);
+
+        public static int CreateStream(string Url, int Offset, BassFlags Flags, DownloadProcedure Procedure, IntPtr User = default(IntPtr))
+        {
+            var h = BASS_AC3_StreamCreateURL(Url, Offset, Flags | BassFlags.Unicode, Procedure, User);
+
+            if (h != 0)
+                Extensions.ChannelReferences.Add(h, 0, Procedure);
+
+            return h;
+        }
+    }
+}
---
+++ src/PInvoke/Plugins/Plugin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace ManagedBass
+{
+    /// <summary>
+    /// Wraps Plugins AddOns.
+    /// </summary>
+    public class Plugin
+    {
+        public string DllName { get; }
+        int _hPlugin;
+        PluginInfo? _info;
+
+        public Version Version
+        {
+            get
+            {
+                if (_info != null)
+                    return _info.Value.Version;
+
+                Load();
+
+                _info = Bass.PluginGetInfo(_hPlugin);
+
+                return _info.Value.Version;
+            }
+        }
+
+        public PluginFormat[] SupportedFormats
+        {
+            get
+            {
+                if (_info == null)
+                {
+                    Load();
+
+                    _info = Bass.PluginGetInfo(_hPlugin);
+                }
+
+                return _info.Value.Formats;
+            }
+        }
+
+        internal Plugin(string DllName) { this.DllName = DllName; }
+
+        /// <summary>
+        /// Load the plugin into memory.
+        /// <param name="Folder">Folder to load the plugin from... <see langword="null"/> (default), Load from Current Directory.</param>
+        /// </summary>
+        public void Load(string Folder = null)
+        {
+            if (_hPlugin != 0)
+                return;
+
+            _hPlugin = Bass.PluginLoad(Folder != null ? Path.Combine(Folder, DllName) : DllName);
+            
+            if (_hPlugin == 0)
+                throw new DllNotFoundException(DllName);
+        }
+
+        public void Unload()
+        {
+            if (_hPlugin != 0 && Bass.PluginFree(_hPlugin))
+                _hPlugin = 0;
+        }
+    }
+}
