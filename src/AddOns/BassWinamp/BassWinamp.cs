f9b1c9e5d4615f4c81f3da56e2385047f1d73d37
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-04-16 20:57:04 +0530

    update

--- src/AddOns/BassWinamp/BassWinamp.cs
+++ src/AddOns/BassWinamp/BassWinamp.cs
@@ -14,32 +14,11 @@ namespace ManagedBass.Winamp
     {
         const string DllName = "bass_winamp";
 
-        static IntPtr hLib;
-
-        /// <summary>
-        /// Load this library into Memory.
-        /// </summary>
-        /// <param name="Folder">Directory to Load from... <see langword="null"/> (default) = Load from Current Directory.</param>
-        /// <returns><see langword="true" />, if the library loaded successfully, else <see langword="false" />.</returns>
-        /// <remarks>
-        /// <para>
-        /// An external library is loaded into memory when any of its methods are called for the first time.
-        /// This results in the first method call being slower than all subsequent calls.
-        /// </para>
-        /// <para>
-        /// Some BASS libraries and add-ons may introduce new options to the main BASS lib like new parameters.
-        /// But, before using these new options the respective library must be already loaded.
-        /// This method can be used to make sure, that this library has been loaded.
-        /// </para>
-        /// </remarks>
-        public static bool Load(string Folder = null) => (hLib = DynamicLibrary.Load(DllName, Folder)) != IntPtr.Zero;
-
-        /// <summary>
-        /// Unloads this library from Memory.
-        /// </summary>
-        /// <returns><see langword="true" />, if the library unloaded successfully, else <see langword="false" />.</returns>
-        public static bool Unload() => DynamicLibrary.Unload(hLib);
-
+        public static void Load()
+        {
+            CreateStream("", 0);
+        }
+        
         /// <summary>
         /// Winamp input timeout (in milliseconds) to wait until timing out, because the plugin is not using the output system.
         /// </summary>
