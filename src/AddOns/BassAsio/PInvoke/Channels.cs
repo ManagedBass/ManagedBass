0729135be446f9b3a859ed2712c58dad54610141
olitee <oli@trenouth.co.uk>
2019-07-15 13:28:28 +0100

    Added support for BASS_ASIO_ChannelEnableBASS

--- src/AddOns/BassAsio/PInvoke/Channels.cs
+++ src/AddOns/BassAsio/PInvoke/Channels.cs
@@ -30,6 +30,37 @@ namespace ManagedBass.Asio
         [DllImport(DllName, EntryPoint = "BASS_ASIO_ChannelEnable")]
         public static extern bool ChannelEnable(bool Input, int Channel, AsioProcedure Procedure, IntPtr User = default(IntPtr));
 
+        /// <summary>
+        /// Enables a channel, and sets it to use a BASS channel.
+        /// </summary>
+        /// <param name="Input">Dealing with an input channel? <see langword="false" /> = an output channel.</param>
+        /// <param name="Channel">The input/output channel number... 0 = first.</param>
+        /// <param name="Handle">The BASS channel handle.</param>
+        /// <param name="Join">Join the next ASIO channels according to the number of audio channels in the BASS channel?</param>
+        /// <returns>If succesful, then <see langword="true" /> is returned, else <see langword="false" /> is returned. Use <see cref="LastError" /> to get the error code.</returns>
+        /// <remarks>
+        /// <para>
+        /// This function allows BASS channels to be used directly, without needing an ASIOPROC callback function. The ASIO channel's format and rate are set accordingly.
+        /// If the BASS channel is not mono then multiple ASIO channels should also be joined accordingly. That can be done automatically via the join parameter, or manually
+        /// with BASS_ASIO_ChannelJoin. If the device does not have enough channels, the BASSmix add-on can be used to downmix the BASS channel.
+        /// </para>
+        /// <para>
+        /// In the case of output channels, the BASS channel must have the BASS_STREAM_DECODE flag set. In the case of input channels, the BASS channel must be a "push" stream,
+        /// created with BASS_StreamCreate and STREAMPROC_PUSH, which will receive the data from the input channel(s).
+        /// <para>
+        /// Raw DSD streams are supported (with the BASSDSD add-on) but the device needs to have been successfully set to DSD mode first with BASS_ASIO_SetDSD.
+        /// The device's sample rate should also be set to the DSD stream's rate (its BASS_ATTRIB_DSD_RATE attribute) via BASS_ASIO_SetRate.
+        /// </para>
+        /// </remarks>
+        /// <exception cref="Errors.Init"><see cref="Init" /> has not been successfully called.</exception>
+        /// <exception cref="Errors.Start">The device has been started - it needs to be stopped before (dis)enabling channels.</exception>
+        /// <exception cref="Errors.Parameter">The <paramref name="Input" /> and <paramref name="Channel" /> combination is invalid.</exception>
+        /// <exception cref="Errors.Handle">Handle is invalid</exception>
+        /// <exception cref="Errors.Format">8-bit BASS channels are not supported; the BASS_SAMPLE_FLOAT flag can be used to avoid them.</exception>
+        /// <exception cref="Errors.NoChannel">The device does not have enough channels to accommodate the BASS channel.</exception>
+        [DllImport(DllName, EntryPoint = "BASS_ASIO_ChannelEnableBASS")]
+        public static extern bool ChannelEnableBass(bool Input, int Channel, int Handle, bool Join);
+
 		/// <summary>
 		/// Enables an output channel, and makes it mirror another channel.
 		/// </summary>
