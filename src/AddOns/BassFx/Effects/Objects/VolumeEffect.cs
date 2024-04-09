2bf94d5d3445d7f88bb6db19dcea5afc8c86bf8c
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2024-04-09 10:32:58 -0400

    Make access modifiers consistent

--- src/AddOns/BassFx/Effects/Objects/VolumeEffect.cs
+++ src/AddOns/BassFx/Effects/Objects/VolumeEffect.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ManagedBass.Fx
 {
-    public class Volume : Effect<VolumeParameters>
+    public sealed class Volume : Effect<VolumeParameters>
     {
         /// <summary>
         /// The new volume level... 0 = silent, 1.0 = normal, above 1.0 = amplification. The default value is 1.
