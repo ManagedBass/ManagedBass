b6ced792f0deca280a1eff615e449cad5db3b474
Oli Trenouth <oli@trenouth.co.uk>
2025-09-01 23:08:46 +0100

    Refactored Bass, Bass.Enc and Bass.Midi projects to simplify cross-platform support (#131)
    * Reverted to support for .NET Framework 451 and .NET Standard 1.4. Fixed nuspec file for better support for ios.
    
    * Various target framework fixes:
    1. Added support for .NET Framework 4.5
    2. Bumped .NET Standard support from 1.4 to 2.0
    3. Added support for .NET 8+
    
    * Updated README.md with framework/platform support details and clarified download instructions. Added note on iOS static linking requirement.
    
    * Restructured Bass, BassEnc and BassMidi projects to integrate all cross platform concerns into a single project.
    
    iOS-specific code has been moved into a PlatformSpecific/iOS folder within those projects.
    
    iOS-specific builds are now created by specifying the 'iOS' platform at build time.
    
    Also added new '__STATIC_LINKING__' constant to enable specific build for iOS < v8.0 where static linking is still required.
    
    * Removed `.nuspec` files for Bass, BassEnc, and BassMidi projects and migrated package metadata into respective `.csproj` files. Updated build pipeline to eliminate custom `.nuspec` packaging and streamline multi-target framework builds.
    
    * Converted github.action package task to shell script, and added a version fallback if not present in pipeline.
    
    * Fixes for github action versioning and fallback
    
    * Updated readme
    
    * Added conditional compilation for .NET Framework and .NET Core in BassMarshal methods. Adjusted `PtrToStructure` and `SizeOf` implementations for framework-specific differences in `Marshal` APIs.
    
    * Simplified `SizeOf` method by removing conditional compilation and using a unified `object` parameter.

---
+++ src/Bass/Portable/BassMarshal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace ManagedBass;
+
+/// <summary>
+/// A Bass Marshaling class, used to add multi-framework support for common marshalling of types.
+/// </summary>
+public static class BassMarshal
+{
+    /// <summary>Marshals data from an unmanaged block of memory to a newly allocated managed object of the specified type.</summary>
+    /// <param name="ptr">A pointer to an unmanaged block of memory.</param>
+    /// <returns>A managed object containing the data pointed to by the <paramref name="ptr" /> parameter.</returns>
+    /// <exception cref="T:System.ArgumentException">The <paramref name="structureType" /> parameter layout is not sequential or explicit.
+    /// -or-
+    /// The <paramref name="structureType" /> parameter is a generic type definition.</exception>
+    /// <exception cref="T:System.ArgumentNullException">
+    /// <paramref name="structureType" /> is <see langword="null" />.</exception>
+    /// <exception cref="T:System.MissingMethodException">The class specified by <paramref name="structureType" /> does not have an accessible default constructor.</exception>
+    public static T PtrToStructure<T>(IntPtr ptr)
+    {
+#if NETFRAMEWORK
+        return (T)Marshal.PtrToStructure(ptr, typeof(T));
+#else
+        return Marshal.PtrToStructure<T>(ptr);
+#endif
+    }
+
+    /// <summary>Returns the size of an unmanaged type in bytes.</summary>
+    /// <param name="t">The type whose size is to be returned.</param>
+    /// <returns>The size of the specified type in unmanaged code.</returns>
+    /// <exception cref="T:System.ArgumentException">The <paramref name="t" /> parameter is a generic type definition.</exception>
+    /// <exception cref="T:System.ArgumentNullException">The <paramref name="t" /> parameter is <see langword="null" />.</exception>
+    public static int SizeOf<T>() =>
+#if NETFRAMEWORK
+        Marshal.SizeOf(typeof(T));
+#else
+        Marshal.SizeOf<T>();
+#endif
+
+    /// <summary>Returns the size of an unmanaged type in bytes.</summary>
+    /// <param name="obj">The type whose size is to be returned.</param>
+    /// <returns>The size of the specified type in unmanaged code.</returns>
+    /// <exception cref="T:System.ArgumentException">The <paramref name="t" /> parameter is a generic type definition.</exception>
+    /// <exception cref="T:System.ArgumentNullException">The <paramref name="t" /> parameter is <see langword="null" />.</exception>
+    public static int SizeOf(object obj) => Marshal.SizeOf(obj);
+}
